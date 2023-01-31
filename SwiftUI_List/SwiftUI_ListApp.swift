ca71f300006f89b25f0f02d989d886783f311258
jungguen89 <leral3@nate.com>
2023-01-31 14:29:57 +0900

    Initial Commit

---
+++ SwiftUI_List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI_List
+//
+//  Created by 권정근 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
